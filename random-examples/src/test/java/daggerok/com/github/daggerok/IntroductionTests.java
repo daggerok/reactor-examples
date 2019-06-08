a8f3218a1e786a92367d24c0e9803a2a59005761
Maksim Kostromin <daggerok@gmail.com>
2019-06-09 01:07:49 +0300

    Update README.adoc

--- random-examples/src/test/java/daggerok/com/github/daggerok/IntroductionTests.java
+++ random-examples/src/test/java/daggerok/com/github/daggerok/IntroductionTests.java
@@ -1,30 +1,23 @@
 package daggerok.com.github.daggerok;
 
-import daggerok.extensions.CaptureSystemOutput;
-import daggerok.extensions.CaptureSystemOutput.OutputCapture;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-
-import static org.hamcrest.CoreMatchers.containsString;
+import reactor.test.StepVerifier;
 
 @Slf4j
-@CaptureSystemOutput
 @DisplayName("Reactor 3 introduction: https://www.codingame.com/playgrounds/929/reactive-programming-with-reactor-3/Intro?fbclid=IwAR3aKiqkI1_xUr5zrwqzQz-4djGdtRbbOuBR8FC9wWfxgH33MUBvvm3fuFQ")
 class IntroductionTests {
 
   @Test
   @DisplayName("Should create an expected subscription")
-  void should_create_an_expected_subscription(final OutputCapture output) {
+  void should_create_an_expected_subscription() {
 
-    Flux.just("123")
-        .map(s -> "A" + s)
-        .subscribe(System.out::println);
-
-    output.expect(containsString("A"));
-    output.expect(containsString("123"));
-    output.expect(containsString("A123"));
-
+    Flux<String> flux = Flux.just("123")
+                            .map(s -> "A" + s);
+    StepVerifier.create(flux)
+                .expectNext("A123")
+                .verifyComplete();
   }
 }
--- yet-another-reactor-testing/README.md
+++ yet-another-reactor-testing/README.md
@@ -1,4 +1,8 @@
 # yet-another-reactor-testing
 Yet another...
 
+## resources
+
+* [Reactive Performance by Oleh Dokuka](https://www.youtube.com/watch?v=pyqIpqCt8PU)
+
 NOTE: _This project has been based on [GitHub: daggerok/main-starter](https://github.com/daggerok/main-starter)_
