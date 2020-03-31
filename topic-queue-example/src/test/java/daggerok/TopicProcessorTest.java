c130cb5b08dd5003aafb90aae268269b83f816a5
Maksim Kostromin <daggerok@gmail.com>
2020-04-01 02:10:20 +0300

    I don't believe it (C) whatthecommit.com

--- topic-queue-example/src/test/java/daggerok/TopicProcessorTest.java
+++ topic-queue-example/src/test/java/daggerok/TopicProcessorTest.java
@@ -2,13 +2,47 @@ package daggerok;
 
 import io.vavr.control.Try;
 import org.junit.jupiter.api.Test;
-import reactor.core.publisher.TopicProcessor;
+import reactor.core.publisher.FluxProcessor;
 
 class TopicProcessorTest {
 
   @Test
-  void test() {
-    TopicProcessor<String> topicProcessor = TopicProcessor.create();
+  void test_deprecated() {
+    System.out.println("deprecated");
+
+    FluxProcessor<String, String> topicProcessor = reactor.core.publisher.TopicProcessor.create();
+    //
+    topicProcessor.subscribe(s -> System.out.println("A: " + s));
+    topicProcessor.subscribe(s -> System.out.println("B: " + s));
+    //
+    topicProcessor.onNext("a");
+    topicProcessor.onNext("b");
+    topicProcessor.onNext("c");
+    //
+    Try.run(() -> Thread.sleep(1234));
+  }
+
+  @Test
+  void test_replacement() {
+    System.out.println("replacement");
+
+    FluxProcessor<String, String> topicProcessor = reactor.extra.processor.TopicProcessor.create();
+    //
+    topicProcessor.subscribe(s -> System.out.println("A: " + s));
+    topicProcessor.subscribe(s -> System.out.println("B: " + s));
+    //
+    topicProcessor.onNext("a");
+    topicProcessor.onNext("b");
+    topicProcessor.onNext("c");
+    //
+    Try.run(() -> Thread.sleep(1234));
+  }
+
+  @Test
+  void test_alternative() {
+    System.out.println("alternative");
+
+    FluxProcessor<String, String> topicProcessor = reactor.core.publisher.DirectProcessor.create();
     //
     topicProcessor.subscribe(s -> System.out.println("A: " + s));
     topicProcessor.subscribe(s -> System.out.println("B: " + s));
--- topic-queue-example/src/test/java/daggerok/WorkQueueProcessorTest.java
+++ topic-queue-example/src/test/java/daggerok/WorkQueueProcessorTest.java
@@ -2,13 +2,32 @@ package daggerok;
 
 import io.vavr.control.Try;
 import org.junit.jupiter.api.Test;
-import reactor.core.publisher.WorkQueueProcessor;
+import reactor.core.publisher.FluxProcessor;
+import reactor.extra.processor.WorkQueueProcessor;
 
 class WorkQueueProcessorTest {
 
   @Test
-  void test() {
-    WorkQueueProcessor<String> workQueueProcessor = WorkQueueProcessor.create();
+  void test_replacement() {
+    System.out.println("replacement");
+
+    FluxProcessor<String, String> workQueueProcessor = reactor.extra.processor.WorkQueueProcessor.create();
+    //
+    workQueueProcessor.subscribe(s -> System.out.println("A: " + s));
+    workQueueProcessor.subscribe(s -> System.out.println("B: " + s));
+    //
+    workQueueProcessor.onNext("a");
+    workQueueProcessor.onNext("b");
+    workQueueProcessor.onNext("c");
+    //
+    Try.run(() -> Thread.sleep(1234));
+  }
+
+  @Test
+  void test_deprecated() {
+    System.out.println("deprecated");
+
+    FluxProcessor<String, String> workQueueProcessor = reactor.core.publisher.WorkQueueProcessor.create();
     //
     workQueueProcessor.subscribe(s -> System.out.println("A: " + s));
     workQueueProcessor.subscribe(s -> System.out.println("B: " + s));
